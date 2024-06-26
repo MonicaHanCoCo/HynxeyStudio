6eaee9f43bb51d5caadc3c8c4e0aa965d943f3af
MonicaHanCoCo <353433836@qq.com>
2024-06-26 16:12:10 +0800

    v1

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+// import './App.css'
+import React from 'react';
+// components
+import Banner from './components/Banner.jsx';
+import Header from './components/Header.jsx';
+import Nav from './components/Nav.jsx';
+import About from './components/About.jsx';
+import Services from './components/Services.jsx';
+import Work from './components/Work.jsx';
+import Contact from './components/Contact.jsx';
+
+const App = () => {
+  return (
+    <div className='bg-site bg-no-repeat bg-cover overflow-hidden'>
+      <Header />
+      <Banner />
+      <Nav />
+      <About />
+      <Services />
+      <Work />
+      <Contact />
+      {/* <div className='h-[4000px]'></div> */}
+    </div>
+  );
+};
+
+export default App;
